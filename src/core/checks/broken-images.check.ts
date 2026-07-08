76d6fe433f18565fe494df04a69a0ae5123c8d06
BogdanBedrinec <bogdanbedrinec@gmail.com>
2026-07-08 23:21:58 +0200

    Clean broken image detection

--- src/core/checks/broken-images.check.ts
+++ src/core/checks/broken-images.check.ts
@@ -8,19 +8,24 @@ export type BrokenImagesCheckResult = {
 export async function checkBrokenImages(
   page: Page
 ): Promise<BrokenImagesCheckResult> {
-  const brokenImages = await page.locator("img").evaluateAll((images) =>
-    images
+  const brokenImages = await page.locator("img").evaluateAll((images) => {
+    const srcList = images
       .map((image) => {
         const img = image as HTMLImageElement;
 
         return {
-          src: img.getAttribute("src"),
+          src: img.currentSrc || img.getAttribute("src"),
           isBroken: !img.complete || img.naturalWidth === 0,
         };
       })
       .filter((image) => image.isBroken && Boolean(image.src))
       .map((image) => image.src as string)
-  );
+      .filter((src) => {
+        return !src.includes("cdninstagram.com") && !src.startsWith("data:");
+      });
+
+    return Array.from(new Set(srcList));
+  });
 
   return {
     brokenImagesCount: brokenImages.length,
